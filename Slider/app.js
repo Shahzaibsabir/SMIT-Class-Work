ff47bef2a020bc48a32ef6319f986727248ca746
SHAHZAIB KHAN <56146605+Shahzaibsabir@users.noreply.github.com>
2023-10-18 18:12:59 +0500

    Add files via upload
    slider folder

---
+++ Slider/app.js
@@ -0,0 +1,28 @@
+
+
+// const userData = [
+//     {
+//         name: "Hassan",
+//         About: "WEb Developer, Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quisquam corporis vero ipsa eveniet laborum, facere quia temporibus natus odio esse."
+//     },
+//     {
+//         name: "Talha",
+//         About: "WEb Developer, Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quisquam corporis vero ipsa eveniet laborum, facere quia temporibus natus odio esse."
+//     },
+//     {
+//         name: "Furqan",
+//         About: "WEb Developer, Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quisquam corporis vero ipsa eveniet laborum, facere quia temporibus natus odio esse."
+//     },
+//     {
+//         name: "Shahzaib Khan",
+//         About: "WEb Developer, Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quisquam corporis vero ipsa eveniet laborum, facere quia temporibus natus odio esse."
+//     }
+// ]
+// var numArr = 0++
+
+
+// function nextHandler() {
+
+
+
+// }
