74b679b215c390147df31841b5081e91729e444a
SHAHZAIB KHAN <56146605+Shahzaibsabir@users.noreply.github.com>
2023-10-29 22:18:32 +0500

    Update Loginpg.js
    Js code

--- Loginpg.js
+++ Loginpg.js
@@ -1,1 +1,12 @@
 
+var attempt = 3; //Variable to count number of attempts
+............
+attempt --; //Decrementing by one
+.............
+document.getElementById("username").disabled = true;
+document.getElementById("password").disabled = true;
+document.getElementById("submit").disabled = true;
+return false;
+}
+}
+}
