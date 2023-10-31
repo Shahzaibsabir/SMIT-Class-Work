70d8f5899a496c40769894609f3dc479608583d1
SHAHZAIB KHAN <56146605+Shahzaibsabir@users.noreply.github.com>
2023-10-31 10:30:14 -0700

    Update Loginpg.js
     modal add

--- Loginpg.js
+++ Loginpg.js
@@ -124,6 +124,21 @@ span.psw {
   font-size: 35px;
   font-weight: bold;
 }
+
+
+
+<script>
+// Get the modal
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+</script>
+
 </script>
 
 
