03801203d171d58087ea22e5f93f8e28b5c9dd32
SHAHZAIB KHAN <56146605+Shahzaibsabir@users.noreply.github.com>
2023-11-01 21:36:11 +0500

    Update Loginpg.js
    Update Code

--- Loginpg.js
+++ Loginpg.js
@@ -24,7 +24,49 @@ return false;
 }
 
 
+<script>
+// Get the modal
+var modal = document.getElementById('id01');
 
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+
+
+
+    ?
+    // Adding SignUP Code
+
+    <!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+body {font-family: Arial, Helvetica, sans-serif;}
+
+/* Full-width input fields */
+input[type=text], input[type=password] {
+  width: 100%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  display: inline-block;
+  border: 1px solid #ccc;
+  box-sizing: border-box;
+}
+
+/* Set a style for all buttons */
+button {
+  background-color: #04AA6D;
+  color: white;
+  padding: 14px 20px;
+  margin: 8px 0;
+  border: none;
+  cursor: pointer;
+  width: 100%;
+}
+  }
+}
 
 
 <script>
