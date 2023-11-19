d0f9b95a0a3efb37d1597f51d1a7d689186e1e40
SHAHZAIB KHAN <56146605+Shahzaibsabir@users.noreply.github.com>
2023-11-19 21:44:01 +0500

    Update Objects.js
    comment

--- Objects.js
+++ Objects.js
@@ -1,1 +1,25 @@
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Number Increment or Decrement Programm</title>
+
+    <style>
+*{
+    margin: 0px;
+    padding: 0px;
+    box-sizing: border-box;
+}
+
+body {
+
+display: flex;
+justify-content: center;
+align-items: center;
+margin-top: 200px;
+
+}
+
+
